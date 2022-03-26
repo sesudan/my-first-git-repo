a57f171927acaf2509bddd6995d6fe18570af340
Seher Sudan <sudanseher@gmail.com>
2022-03-26 20:00:41 +0000

    second

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,5 +1,7 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("hi");
+        System.out.println("new");
     }
 }
